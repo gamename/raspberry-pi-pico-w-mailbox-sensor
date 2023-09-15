5775fcee7e5187393682b6275f0029dd08bc3a89
gamename <tennis.n.smith@gmail.com>
2023-09-14 20:29:32 -0500

    updated docs

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 """
+
+
+Pico W 3v3/Physical pin #36 ----> reed switch (normally open) ----> Pico W GPIO Pin #22/Physical Pin #29
+
+
+
 """
 import time
 
@@ -8,22 +14,20 @@ from machine import Pin, reset, WDT
 
 import secrets
 
-CONTACT_PIN = 22
+CONTACT_PIN = 22  # GPIO pin #22, physical pin #29
 
 # How long to sleep between network connection attempts?
 NETWORK_SLEEP_INTERVAL = 5  # seconds
 
-# How long to pause before checking for the garage being open?
-MINUTES = 10
+# How many times should we try to start the network connection?
+MAX_NETWORK_CONNECTION_ATTEMPTS = 20
 
-# Now, calculate the pause minutes
-PAUSE_MINUTES = 60 * MINUTES
-
-MAX_NETWORK_CONNECTION_ATTEMPTS = 10
-
+# If watchdog is not 'fed' in 8 seconds, initiate a hard reset
 WATCHDOG_TIMEOUT = 8000  # 8 seconds
 
-STATE_TIMER = 300  # seconds
+# Time to wait while the door has been opened
+DOOR_OPEN_STATE_TIMER = 300  # seconds
+
 
 def wifi_connect(dog, wlan):
     """
@@ -33,7 +37,7 @@ def wifi_connect(dog, wlan):
     :param wlan - a wifi network handle
 
     Returns:
-        True when successful
+        True when successful, hard reset if not
     """
     led = Pin("LED", Pin.OUT)
     led.off()
@@ -66,14 +70,16 @@ def main():
         print("starting event loop")
         while True:
             if not reed_switch_on.value():
-                print("LOW")
+                print("Mailbox door opened!")
                 requests.post(secrets.REST_API_URL, headers={'content-type': 'application/json'})
                 state_counter = 0
-                while state_counter < STATE_TIMER:
+                # Set a timer to keep us from re-sending SMS notices
+                while state_counter < DOOR_OPEN_STATE_TIMER:
                     print("in LOW state")
                     state_counter += 1
                     time.sleep(1)
                     watchdog.feed()
+                    # If the mailbox door is closed, exit the state timer
                     if reed_switch_on.value():
                         print("exiting LOW state")
                         break
