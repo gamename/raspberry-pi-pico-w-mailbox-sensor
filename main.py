5a92a975885336b3941e456267c353a817fc9780
gamename <tennis.n.smith@gmail.com>
2023-09-18 13:12:37 -0500

    add exponential backoff timer

--- main.py
+++ main.py
@@ -25,8 +25,18 @@ MAX_NETWORK_CONNECTION_ATTEMPTS = 20
 # If watchdog is not 'fed' in 8 seconds, initiate a hard reset
 WATCHDOG_TIMEOUT = 8000  # 8 seconds
 
-# Time to wait while the door has been opened
-DOOR_OPEN_STATE_TIMER = 28800  # seconds (8 hours)
+ONE_DAY = 86400  # seconds
+
+DEFAULT_MINUTES_DELAY = 3
+
+DELAY_EXPONENT = 3
+
+
+def power_generator(base, exponent):
+    result = 1
+    for _ in range(exponent + 1):
+        yield result
+        result *= base
 
 
 def wifi_connect(dog, wlan):
@@ -41,15 +51,15 @@ def wifi_connect(dog, wlan):
     """
     led = Pin("LED", Pin.OUT)
     led.off()
-    wlan.config(pm=wlan.PM_NONE)  # turn OFF power save mode
+    # wlan.config(pm=wlan.PM_NONE)  # turn OFF power save mode
     wlan.active(True)
     time.sleep(NETWORK_SLEEP_INTERVAL)
     dog.feed()
     print("attempting network restart")
     counter = 0
+    wlan.connect(secrets.SSID, secrets.PASSWORD)
     while not wlan.isconnected():
         print(f'attempt: {counter}')
-        wlan.connect(secrets.SSID, secrets.PASSWORD)
         time.sleep(NETWORK_SLEEP_INTERVAL)
         counter += 1
         if counter > MAX_NETWORK_CONNECTION_ATTEMPTS:
@@ -61,17 +71,20 @@ def wifi_connect(dog, wlan):
     return True
 
 
-def handle_door_open_state(watchdog, reed_switch):
+def handle_door_open_state(watchdog, reed_switch, delay_minutes=3):
     """
     Deal with the situation where the mailbox door has been opened
 
     :param watchdog: A watchdog timer
     :param reed_switch: A reed switch handle
+    :param delay_minutes: how long to delay before we return
     :return: Nothing
     """
     state_counter = 0
+    delay_seconds = delay_minutes * 60
     # Set a timer to keep us from re-sending SMS notices
-    while state_counter < DOOR_OPEN_STATE_TIMER:
+    print(f'will delay for {delay_seconds} seconds')
+    while state_counter < delay_seconds:
         print("in LOW state")
         state_counter += 1
         time.sleep(1)
@@ -90,11 +103,16 @@ def main():
     watchdog.feed()
     if wifi_connect(watchdog, wlan):
         print("starting event loop")
+        power_value = power_generator(DEFAULT_MINUTES_DELAY, DELAY_EXPONENT)
+        start_time = time.time()
         while True:
             if not reed_switch.value():
                 print("Mailbox door opened!")
                 requests.post(secrets.REST_API_URL, headers={'content-type': 'application/json'})
-                handle_door_open_state(watchdog, reed_switch)
+                handle_door_open_state(watchdog, reed_switch, next(power_value))
+                elapsed_time = int(time.time() - start_time)
+                if elapsed_time > ONE_DAY:
+                    power_value = power_generator(DEFAULT_MINUTES_DELAY, DELAY_EXPONENT)
 
             if not wlan.isconnected():
                 print("restart network connection!")
