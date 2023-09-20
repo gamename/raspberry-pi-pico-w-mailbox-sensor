6b55ddf8ca310f5054a3520bd6534e9808daf248
gamename <tennis.n.smith@gmail.com>
2023-09-19 20:21:36 -0500

    refactored ota to be clearer

--- ota.py
+++ ota.py
@@ -6,16 +6,30 @@ import machine
 import urequests
 
 
+def process_version_url(repo_url, filename):
+    """ Convert the file's url to its assoicatied version based on Github's oid management."""
+
+    # Necessary URL manipulations
+    version_url = repo_url.replace("raw.githubusercontent.com", "github.com")  # Change the domain
+    version_url = version_url.replace("/", "§", 4)  # Temporary change for upcoming replace
+    version_url = version_url.replace("/", "/latest-commit/", 1)  # Replacing for latest commit
+    version_url = version_url.replace("§", "/", 4)  # Rollback Temporary change
+    version_url = version_url + filename  # Add the targeted filename
+
+    return version_url
+
+
 class OTAUpdater:
     """ This class handles OTA updates. It checks for updates, downloads and installs them."""
 
     NEW_CODE = 'latest_code.py'
+    JSON_VERSION_FILE = 'version.json'
 
     def __init__(self, repo_url, filename):
         self.filename = filename
         self.repo_url = repo_url
 
-        self.version_url = self.process_version_url(repo_url, filename)  # Process the new version url
+        self.version_url = process_version_url(repo_url, filename)  # Process the new version url
         self.firmware_url = repo_url + filename  # Removal of the 'main' branch to allow different sources
 
         self.current_version = None
@@ -23,50 +37,36 @@ class OTAUpdater:
         self.latest_version = None
 
         # get the current version (stored in version.json)
-        if 'version.json' in os.listdir():
-            with open('version.json') as f:
+        if self.JSON_VERSION_FILE in os.listdir():
+            with open(self.JSON_VERSION_FILE) as f:
                 self.current_version = json.load(f)['version']
             print(f"Current device firmware version is '{self.current_version}'")
 
         else:
             self.current_version = "0"
             # save the current version
-            with open('version.json', 'w') as f:
+            with open(self.JSON_VERSION_FILE, 'w') as f:
                 json.dump({'version': self.current_version}, f)
 
-
-    def process_version_url(self, repo_url, filename):
-        """ Convert the file's url to its assoicatied version based on Github's oid management."""
-
-        # Necessary URL manipulations
-        version_url = repo_url.replace("raw.githubusercontent.com", "github.com")  # Change the domain
-        version_url = version_url.replace("/", "§", 4)  # Temporary change for upcoming replace
-        version_url = version_url.replace("/", "/latest-commit/", 1)  # Replacing for latest commit
-        version_url = version_url.replace("§", "/", 4)  # Rollback Temporary change
-        version_url = version_url + filename  # Add the targeted filename
-
-        return version_url
-
-    def fetch_latest_code(self) -> bool:
-        """ Fetch the latest code from the repo, returns False if not found."""
+    def download_latest_firmware(self) -> bool:
+        """ Fetch the latest code from the repo."""
         status = False
 
         # Fetch the latest code from the repo.
         response = urequests.get(self.firmware_url)
-        if response.status_code == 200:
+        if response.status_code != 200:
+            print("OTA: Error pulling github code")
+        else:
             print(f'OTA: Fetched latest firmware code, status: {response.status_code}, -  {response.text}')
-
             # Save the fetched code to memory
             self.latest_code = response.text
             status = True
-        else:
-            print("OTA: no new code found")
 
         return status
 
-    def update_code(self):
+    def update_local_firmware(self):
         """ Update the code."""
-        print("OTA: Update the code")
+        print("OTA: Update the local code")
 
         # Save the fetched code and update the version file to latest version.
         with open(self.NEW_CODE, 'w') as f:
@@ -76,7 +76,7 @@ class OTAUpdater:
         self.current_version = self.latest_version
 
         # save the current version
-        with open('version.json', 'w') as f:
+        with open(self.JSON_VERSION_FILE, 'w') as f:
             json.dump({'version': self.current_version}, f)
 
         # free up some memory
@@ -89,7 +89,7 @@ class OTAUpdater:
         sleep(0.25)
         machine.reset()  # Reset the device to run the new code.
 
-    def check_for_updates(self):
+    def updates_available(self) -> bool:
         """ Check if updates are available."""
 
         print('OTA: Checking for latest version...')
@@ -109,8 +109,7 @@ class OTAUpdater:
 
     def download_and_install_update_if_available(self):
         """ Check for updates, download and install them."""
-        if self.check_for_updates() and self.fetch_latest_code():
-            print("OTA: latest code found and fetched")
-            self.update_code()
+        if self.updates_available() and self.download_latest_firmware():
+            self.update_local_firmware()
         else:
             print('OTA: No new updates available.')
