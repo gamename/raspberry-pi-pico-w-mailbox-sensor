2797ea97e9b2d205a65bf1a856b0a0d91329fb0c
gamename <tennis.n.smith@gmail.com>
2023-09-22 07:31:24 -0500

    took out WDT. Ran up github query counts

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 import network
 import urequests as requests
-from machine import Pin, reset, WDT
+from machine import Pin, reset
 
 import secrets
 from ota import OTAUpdater
@@ -19,14 +19,8 @@ NETWORK_SLEEP_INTERVAL = 3  # seconds
 # How many times should we try to start the network connection?
 MAX_NETWORK_CONNECTION_ATTEMPTS = 10
 
-# If watchdog is not 'fed' in 8 seconds, initiate a hard reset
-WATCHDOG_TIMEOUT = 8000  # 8 seconds
-
-# Reset our backoff algorithm after a day has elapsed
-ONE_DAY = 86400  # seconds
-
-# How long should we delay between retries?
-DOOR_OPEN_BACKOFF_DELAY_MINUTES = 3
+# Generate exponentially longer backoff timers starting with this base value
+BACKOFF_DELAY_BASE_VALUE = 3
 
 # Define where we get our updates when we pull them Over The Air (OTA)
 OTA_UPDATE_GITHUB_ORGANIZATION = 'gamename'
@@ -46,21 +40,19 @@ def exponent_generator(base):
         yield base ** i
 
 
-def wifi_connect(dog, wlan):
+def wifi_connect(wlan):
     """
     Connect to Wi-Fi
 
-    :param dog - a watchdog timer
     :param wlan - a Wi-Fi network handle
 
     Returns:
-        True when successful, hard reset if not
+        Nothing
     """
     led = Pin("LED", Pin.OUT)
     led.off()
     wlan.active(True)
     time.sleep(NETWORK_SLEEP_INTERVAL)
-    dog.feed()
     print("Attempting network restart")
     counter = 0
     wlan.connect(secrets.SSID, secrets.PASSWORD)
@@ -72,80 +64,56 @@ def wifi_connect(dog, wlan):
             print("Network connection attempts exceeded! Restarting")
             time.sleep(0.5)
             reset()
-        dog.feed()
     led.on()
     print("Successfully connected to network")
-    return True
 
 
-def handle_door_open_state(watchdog, reed_switch, delay_minutes):
+def handle_door_open_state(reed_switch, delay_minutes):
     """
-    Deal with the situation where the mailbox door has been opened
+    Deal with the situation where the mailbox door has been opened, but may
+    or may not have been closed.
 
-    :param watchdog: A watchdog timer
     :param reed_switch: A reed switch handle
     :param delay_minutes: how long to delay before we return
     :return: Nothing
     """
+    requests.post(secrets.REST_API_URL, headers={'content-type': 'application/json'})
+    # Set a backoff timer to keep us from re-sending SMS notices
+    print(f'Backoff delay for {delay_minutes} minutes')
     state_counter = 0
-    # Set a timer to keep us from re-sending SMS notices
-    delay_seconds = delay_minutes * 60
-    print(f'Will delay for {delay_seconds} seconds')
-    while state_counter < delay_seconds:
+    while state_counter < delay_minutes:
         state_counter += 1
-        time.sleep(1)
-        watchdog.feed()
+        time.sleep(60)
         # If the mailbox door is closed, exit the state timer
         if reed_switch.value():
             print("Door CLOSED")
             break
 
 
-def check_network_status(wlan, watchdog):
-    """
-    Check if we are still connected to the network. If not, retry.
-    :param wlan: A network handle
-    :param watchdog: A watchdog handle
-    :return: Nothing
-    """
-    if not wlan.isconnected():
-        print("Restart network connection")
-        wifi_connect(watchdog, wlan)
-
-
 def main():
-    watchdog = WDT(timeout=WATCHDOG_TIMEOUT)
     network.hostname(secrets.HOSTNAME)
     wlan = network.WLAN(network.STA_IF)
-    watchdog.feed()
-    if wifi_connect(watchdog, wlan):
-        reed_switch = Pin(CONTACT_PIN, Pin.IN, Pin.PULL_DOWN)
-        ota_updater = OTAUpdater(OTA_UPDATE_GITHUB_ORGANIZATION, OTA_UPDATE_GITHUB_REPOSITORY, ["main.py"])
-        exponent = exponent_generator(DOOR_OPEN_BACKOFF_DELAY_MINUTES)
-        start_time = time.time()
-        ota_timer = time.time()
-        print("Starting event loop")
-        while True:
-            if not reed_switch.value():
-                print("Door OPEN")
-                watchdog.feed()
-                requests.post(secrets.REST_API_URL, headers={'content-type': 'application/json'})
-                handle_door_open_state(watchdog, reed_switch, next(exponent))
-                elapsed_time = int(time.time() - start_time)
-                if elapsed_time > ONE_DAY:
-                    print("Restart our daily timer")
-                    exponent = exponent_generator(DOOR_OPEN_BACKOFF_DELAY_MINUTES)
-                    start_time = time.time()
+    wifi_connect(wlan)
+    reed_switch = Pin(CONTACT_PIN, Pin.IN, Pin.PULL_DOWN)
+    ota_updater = OTAUpdater(OTA_UPDATE_GITHUB_ORGANIZATION,
+                             OTA_UPDATE_GITHUB_REPOSITORY,
+                             ["main.py"])
+    exponent = exponent_generator(BACKOFF_DELAY_BASE_VALUE)
+    ota_timer = time.time()
+    print("Starting event loop")
+    while True:
+        if not reed_switch.value():
+            print("Door OPEN")
+            handle_door_open_state(reed_switch, next(exponent))
 
-            check_network_status(wlan, watchdog)
+        if not wlan.isconnected():
+            print("Restart network connection")
+            wifi_connect(wlan)
 
-            ota_elapsed = int(time.time() - ota_timer)
-            if ota_elapsed > OTA_CHECK_INTERVAL:
-                watchdog.feed()
-                ota_updater.update_firmware()
-                ota_timer = time.time()
-
-            watchdog.feed()
+        ota_elapsed = int(time.time() - ota_timer)
+        if ota_elapsed > OTA_CHECK_INTERVAL:
+            ota_updater.update_firmware()
+            ota_timer = time.time()
 
 
 if __name__ == "__main__":
