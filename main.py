d64c2766329e77fec08bb2ff7395c396a90fc593
gamename <tennis.n.smith@gmail.com>
2023-09-23 20:32:46 -0500

    set OTA check to 2 mins to help uncover mem leak

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ DOOR_OPEN_BACKOFF_DELAY_BASE_VALUE = 3  # 3 minutes
 OTA_UPDATE_GITHUB_FILES = ["boot.py", "main.py", "ota.py"]
 
 # How often should we check for updates?
-OTA_UPDATE_GITHUB_CHECK_INTERVAL = 3600  # seconds (1 hour)
+OTA_UPDATE_GITHUB_CHECK_INTERVAL = 120  # seconds (1 hour)
 
 # What organization/repo do we pull updates from?
 OTA_UPDATE_GITHUB_ORGANIZATION = 'gamename'
